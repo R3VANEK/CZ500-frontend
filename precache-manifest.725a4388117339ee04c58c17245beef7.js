9937d7d524ff77aaebf27d341f29291334ea345c
Arcywojak <maciej778@op.pl>
2020-05-01 21:37:18 +0200

    Updates

---
+++ precache-manifest.725a4388117339ee04c58c17245beef7.js
@@ -0,0 +1,462 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf58476c1f9e566532868c28ed0cd6c",
+    "url": "/test-gitPage/index.html"
+  },
+  {
+    "revision": "1ab88257d8693a4a9867",
+    "url": "/test-gitPage/static/css/main.b66a2028.chunk.css"
+  },
+  {
+    "revision": "2a3f878773897acd76b9",
+    "url": "/test-gitPage/static/js/2.9a9ceb5d.chunk.js"
+  },
+  {
+    "revision": "a1a88636b752ef668fe9699df3c2d360",
+    "url": "/test-gitPage/static/js/2.9a9ceb5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab88257d8693a4a9867",
+    "url": "/test-gitPage/static/js/main.bb882fca.chunk.js"
+  },
+  {
+    "revision": "891c780a5bbefbe207cf",
+    "url": "/test-gitPage/static/js/runtime-main.41b194fe.js"
+  },
+  {
+    "revision": "81f8e2520ffb5c8e5464f015f4c26e12",
+    "url": "/test-gitPage/static/media/404 copy.81f8e252.svg"
+  },
+  {
+    "revision": "81f8e2520ffb5c8e5464f015f4c26e12",
+    "url": "/test-gitPage/static/media/404.81f8e252.svg"
+  },
+  {
+    "revision": "b5ef7131f5ed85400baf4bb667670f64",
+    "url": "/test-gitPage/static/media/72480187_1139453392927230_7778508410999275520_n copy.b5ef7131.png"
+  },
+  {
+    "revision": "b5ef7131f5ed85400baf4bb667670f64",
+    "url": "/test-gitPage/static/media/72480187_1139453392927230_7778508410999275520_n.b5ef7131.png"
+  },
+  {
+    "revision": "73d53ae8fcb942f05fdbc40b3311b759",
+    "url": "/test-gitPage/static/media/Apple-white.73d53ae8.svg"
+  },
+  {
+    "revision": "ddb8dc89a6aaf0cf9e148d2714d3a7fa",
+    "url": "/test-gitPage/static/media/Apple.ddb8dc89.svg"
+  },
+  {
+    "revision": "4c2a29d6107c40d4376ff9125cd45c35",
+    "url": "/test-gitPage/static/media/Bank.4c2a29d6.svg"
+  },
+  {
+    "revision": "9a68d91665c39fd8addf8f1eb86d5b33",
+    "url": "/test-gitPage/static/media/Birthday.9a68d916.svg"
+  },
+  {
+    "revision": "43cf9b2204f6f3ac1008032d911f2108",
+    "url": "/test-gitPage/static/media/Boy Avatar.43cf9b22.svg"
+  },
+  {
+    "revision": "aef0c142caf1941cf2d26800843243b5",
+    "url": "/test-gitPage/static/media/Delete.aef0c142.svg"
+  },
+  {
+    "revision": "dd2e7ec335f414dbafa10c6b0824ebf3",
+    "url": "/test-gitPage/static/media/Edit.dd2e7ec3.svg"
+  },
+  {
+    "revision": "a9e549960b03d1c0d7408108158cfc03",
+    "url": "/test-gitPage/static/media/Flame.a9e54996.svg"
+  },
+  {
+    "revision": "6524fb7058521c82df726ddf354458ac",
+    "url": "/test-gitPage/static/media/IKONKA2 copy.6524fb70.svg"
+  },
+  {
+    "revision": "6524fb7058521c82df726ddf354458ac",
+    "url": "/test-gitPage/static/media/IKONKA2.6524fb70.svg"
+  },
+  {
+    "revision": "7ddfbf853426c1ca04dd178f8b80c19f",
+    "url": "/test-gitPage/static/media/IKONKA3 copy.7ddfbf85.svg"
+  },
+  {
+    "revision": "7ddfbf853426c1ca04dd178f8b80c19f",
+    "url": "/test-gitPage/static/media/IKONKA3.7ddfbf85.svg"
+  },
+  {
+    "revision": "0f6710b56ff4b53ac0b0406b28673db8",
+    "url": "/test-gitPage/static/media/PlusFaq.0f6710b5.svg"
+  },
+  {
+    "revision": "957a51a20321c898ca29dd9b9a110dbe",
+    "url": "/test-gitPage/static/media/Puzzle-white.957a51a2.svg"
+  },
+  {
+    "revision": "505cd76442a16ba3abc3e053bc83c065",
+    "url": "/test-gitPage/static/media/Puzzle.505cd764.svg"
+  },
+  {
+    "revision": "2ffb87ff16e4fa89b76bd8f7a5cb6409",
+    "url": "/test-gitPage/static/media/Resize.2ffb87ff.svg"
+  },
+  {
+    "revision": "af1cfee0d89a45e2dca7ad300d9a8bcb",
+    "url": "/test-gitPage/static/media/SWINIA copy.af1cfee0.svg"
+  },
+  {
+    "revision": "af1cfee0d89a45e2dca7ad300d9a8bcb",
+    "url": "/test-gitPage/static/media/SWINIA.af1cfee0.svg"
+  },
+  {
+    "revision": "4e086ca094f093a1fe6a333f6a5859e9",
+    "url": "/test-gitPage/static/media/Security.4e086ca0.svg"
+  },
+  {
+    "revision": "de35b5a54e0dbf1805816c571aa9ee3f",
+    "url": "/test-gitPage/static/media/Shoe.de35b5a5.svg"
+  },
+  {
+    "revision": "a925391dbbf8e4d1f787d90e99bb36a5",
+    "url": "/test-gitPage/static/media/Voucher.a925391d.svg"
+  },
+  {
+    "revision": "a9a119e6b492b0dde65ab67decb2ef73",
+    "url": "/test-gitPage/static/media/akcesoriadladzieci-white.a9a119e6.svg"
+  },
+  {
+    "revision": "3f0f1d29b0b54b8e677917e2b97b4bcb",
+    "url": "/test-gitPage/static/media/akcesoriadladzieci.3f0f1d29.svg"
+  },
+  {
+    "revision": "ce1a9397bf8af1111775d677bdf72a80",
+    "url": "/test-gitPage/static/media/baby.ce1a9397.svg"
+  },
+  {
+    "revision": "422cfaeaa76ac0e9b5a5ea9d6bbe67df",
+    "url": "/test-gitPage/static/media/baner2 copy.422cfaea.svg"
+  },
+  {
+    "revision": "422cfaeaa76ac0e9b5a5ea9d6bbe67df",
+    "url": "/test-gitPage/static/media/baner2.422cfaea.svg"
+  },
+  {
+    "revision": "b6c7967fe6d1f9d1633a3026903b0614",
+    "url": "/test-gitPage/static/media/bgp.b6c7967f.png"
+  },
+  {
+    "revision": "820481bb8a564f7cfe0075cf3da0e37b",
+    "url": "/test-gitPage/static/media/circle.820481bb.svg"
+  },
+  {
+    "revision": "407e2397b985ddac6c52b4417c3bd172",
+    "url": "/test-gitPage/static/media/clothes-white.407e2397.svg"
+  },
+  {
+    "revision": "ebca2ee00c0a90c9f0c700a55e3d65ff",
+    "url": "/test-gitPage/static/media/clothes.ebca2ee0.svg"
+  },
+  {
+    "revision": "0d6c3964df26d724636a25d10bc19c24",
+    "url": "/test-gitPage/static/media/cloud.0d6c3964.svg"
+  },
+  {
+    "revision": "21022e73e6a12b0591b2f19ccf2d4c8c",
+    "url": "/test-gitPage/static/media/cog-solid.21022e73.svg"
+  },
+  {
+    "revision": "0905735d6eabe6505b0aa3e7b90c8b08",
+    "url": "/test-gitPage/static/media/color_palette.0905735d.svg"
+  },
+  {
+    "revision": "9ff13a2f90af4272ac6bbb169474b1fd",
+    "url": "/test-gitPage/static/media/contact.9ff13a2f.svg"
+  },
+  {
+    "revision": "2c07245700d1057a88306640756a3304",
+    "url": "/test-gitPage/static/media/czystoscwdomu-white.2c072457.svg"
+  },
+  {
+    "revision": "fec34e3c215101b042e15ad6fa431436",
+    "url": "/test-gitPage/static/media/czystoscwdomu.fec34e3c.svg"
+  },
+  {
+    "revision": "68fce91cfa862760d45d7b5b4bbedd3f",
+    "url": "/test-gitPage/static/media/dlakobietwciazy-white.68fce91c.svg"
+  },
+  {
+    "revision": "ff339798331daa56c8ade649e23ea944",
+    "url": "/test-gitPage/static/media/dlakobietwciazy.ff339798.svg"
+  },
+  {
+    "revision": "a1b06b1fb845068eac160209f9382430",
+    "url": "/test-gitPage/static/media/done.a1b06b1f.svg"
+  },
+  {
+    "revision": "7f29b1495a7d65c813939f2f7f18c4de",
+    "url": "/test-gitPage/static/media/facebook copy.7f29b149.svg"
+  },
+  {
+    "revision": "7f29b1495a7d65c813939f2f7f18c4de",
+    "url": "/test-gitPage/static/media/facebook.7f29b149.svg"
+  },
+  {
+    "revision": "5863de27ca5b9eb2f1a5e3486be389d4",
+    "url": "/test-gitPage/static/media/faq.5863de27.svg"
+  },
+  {
+    "revision": "1d61fe24de63b87cc8cc8d69c83d1205",
+    "url": "/test-gitPage/static/media/fb.1d61fe24.svg"
+  },
+  {
+    "revision": "388c5e85247dbe9d9665fc60705980e9",
+    "url": "/test-gitPage/static/media/film1.388c5e85.mp4"
+  },
+  {
+    "revision": "777c5c7836a15eb67d2e97bd40a14aac",
+    "url": "/test-gitPage/static/media/image.777c5c78.png"
+  },
+  {
+    "revision": "dedfd705c80b25776ac781bb0fd26c0c",
+    "url": "/test-gitPage/static/media/image.dedfd705.svg"
+  },
+  {
+    "revision": "83ce643536f4341bcc3407e489ab52df",
+    "url": "/test-gitPage/static/media/image1.83ce6435.svg"
+  },
+  {
+    "revision": "bc4fcde630ed3cc2af416c64d1275a88",
+    "url": "/test-gitPage/static/media/image2.bc4fcde6.svg"
+  },
+  {
+    "revision": "3bf0069409423b16e0432c12598275b1",
+    "url": "/test-gitPage/static/media/instagram copy.3bf00694.svg"
+  },
+  {
+    "revision": "3bf0069409423b16e0432c12598275b1",
+    "url": "/test-gitPage/static/media/instagram.3bf00694.svg"
+  },
+  {
+    "revision": "8fac244303dfc2316383423ba89b84fe",
+    "url": "/test-gitPage/static/media/item.8fac2443.png"
+  },
+  {
+    "revision": "266a907f965f352a53038dfd8cb622dd",
+    "url": "/test-gitPage/static/media/jakiesKreski.266a907f.svg"
+  },
+  {
+    "revision": "bc1d63480aa5f320ea4bb2232932bbec",
+    "url": "/test-gitPage/static/media/karta copy.bc1d6348.png"
+  },
+  {
+    "revision": "bc1d63480aa5f320ea4bb2232932bbec",
+    "url": "/test-gitPage/static/media/karta.bc1d6348.png"
+  },
+  {
+    "revision": "8905eb42dbebf740a9e6fb6f0d55560f",
+    "url": "/test-gitPage/static/media/klod-1.8905eb42.svg"
+  },
+  {
+    "revision": "8905eb42dbebf740a9e6fb6f0d55560f",
+    "url": "/test-gitPage/static/media/klod-2.8905eb42.svg"
+  },
+  {
+    "revision": "028e7ca328b82e5d9b80b8205b6fd40a",
+    "url": "/test-gitPage/static/media/klodka-fiolet copy.028e7ca3.svg"
+  },
+  {
+    "revision": "028e7ca328b82e5d9b80b8205b6fd40a",
+    "url": "/test-gitPage/static/media/klodka-fiolet.028e7ca3.svg"
+  },
+  {
+    "revision": "8905eb42dbebf740a9e6fb6f0d55560f",
+    "url": "/test-gitPage/static/media/klodka-white copy.8905eb42.svg"
+  },
+  {
+    "revision": "8905eb42dbebf740a9e6fb6f0d55560f",
+    "url": "/test-gitPage/static/media/klodka-white.8905eb42.svg"
+  },
+  {
+    "revision": "0f452f190c2d82a88dd352ec11a93ef4",
+    "url": "/test-gitPage/static/media/kosmetyki-white.0f452f19.svg"
+  },
+  {
+    "revision": "2765bc7abf79cd10d507a579b0b81ba2",
+    "url": "/test-gitPage/static/media/kosmetyki.2765bc7a.svg"
+  },
+  {
+    "revision": "29b5ef9bd2b63f5b602b3308f7db96d2",
+    "url": "/test-gitPage/static/media/logo copy.29b5ef9b.png"
+  },
+  {
+    "revision": "29b5ef9bd2b63f5b602b3308f7db96d2",
+    "url": "/test-gitPage/static/media/logo.29b5ef9b.png"
+  },
+  {
+    "revision": "5e8a9b8377b1ca4c97b1f2e784f5816e",
+    "url": "/test-gitPage/static/media/logo.5e8a9b83.svg"
+  },
+  {
+    "revision": "7c53595dd201b56c59e3c109065f2305",
+    "url": "/test-gitPage/static/media/logout.7c53595d.svg"
+  },
+  {
+    "revision": "90736b597db531f93709f2d1ea8270fa",
+    "url": "/test-gitPage/static/media/mars-solid.90736b59.svg"
+  },
+  {
+    "revision": "f13491ac83df03101c1c494e7ce97c48",
+    "url": "/test-gitPage/static/media/media copy.f13491ac.svg"
+  },
+  {
+    "revision": "f13491ac83df03101c1c494e7ce97c48",
+    "url": "/test-gitPage/static/media/media.f13491ac.svg"
+  },
+  {
+    "revision": "c578e8fd347d91024b057e54b193d522",
+    "url": "/test-gitPage/static/media/message copy.c578e8fd.svg"
+  },
+  {
+    "revision": "c578e8fd347d91024b057e54b193d522",
+    "url": "/test-gitPage/static/media/message.c578e8fd.svg"
+  },
+  {
+    "revision": "e73655659f533fc3fe83b929454e0dc0",
+    "url": "/test-gitPage/static/media/needle.e7365565.jpg"
+  },
+  {
+    "revision": "4a93b99c03f53e8b810b4e9b464cefbd",
+    "url": "/test-gitPage/static/media/nl copy.4a93b99c.png"
+  },
+  {
+    "revision": "4a93b99c03f53e8b810b4e9b464cefbd",
+    "url": "/test-gitPage/static/media/nl.4a93b99c.png"
+  },
+  {
+    "revision": "278999d2238e4039a022ef71d4b95073",
+    "url": "/test-gitPage/static/media/obrazek copy.278999d2.svg"
+  },
+  {
+    "revision": "278999d2238e4039a022ef71d4b95073",
+    "url": "/test-gitPage/static/media/obrazek.278999d2.svg"
+  },
+  {
+    "revision": "a98644c37342e08d94a3707f545b3911",
+    "url": "/test-gitPage/static/media/oferta copy.a98644c3.svg"
+  },
+  {
+    "revision": "a98644c37342e08d94a3707f545b3911",
+    "url": "/test-gitPage/static/media/oferta.a98644c3.svg"
+  },
+  {
+    "revision": "ae9f35e1f0062726ecf23a40e06c15c7",
+    "url": "/test-gitPage/static/media/oko copy.ae9f35e1.svg"
+  },
+  {
+    "revision": "ae9f35e1f0062726ecf23a40e06c15c7",
+    "url": "/test-gitPage/static/media/oko-violet copy.ae9f35e1.svg"
+  },
+  {
+    "revision": "ae9f35e1f0062726ecf23a40e06c15c7",
+    "url": "/test-gitPage/static/media/oko-violet.ae9f35e1.svg"
+  },
+  {
+    "revision": "25f4dfa9b8960e98c33c5ff67251d24b",
+    "url": "/test-gitPage/static/media/oko-white copy.25f4dfa9.svg"
+  },
+  {
+    "revision": "25f4dfa9b8960e98c33c5ff67251d24b",
+    "url": "/test-gitPage/static/media/oko-white.25f4dfa9.svg"
+  },
+  {
+    "revision": "ae9f35e1f0062726ecf23a40e06c15c7",
+    "url": "/test-gitPage/static/media/oko.ae9f35e1.svg"
+  },
+  {
+    "revision": "a05c7974dea093f2fc2c32aef919b41d",
+    "url": "/test-gitPage/static/media/plusCircle.a05c7974.svg"
+  },
+  {
+    "revision": "4e16bd68199ba432c0ab264f88d62cac",
+    "url": "/test-gitPage/static/media/plusCircleAdd.4e16bd68.svg"
+  },
+  {
+    "revision": "6faf4fc05138f07b013bba826f081587",
+    "url": "/test-gitPage/static/media/purchase.6faf4fc0.svg"
+  },
+  {
+    "revision": "4d426f1217b292fb37af0b794dbd58cf",
+    "url": "/test-gitPage/static/media/regulamin.4d426f12.svg"
+  },
+  {
+    "revision": "4bb983f561903a6a5f6da01fc3f9c6f9",
+    "url": "/test-gitPage/static/media/samolot copy.4bb983f5.svg"
+  },
+  {
+    "revision": "4bb983f561903a6a5f6da01fc3f9c6f9",
+    "url": "/test-gitPage/static/media/samolot.4bb983f5.svg"
+  },
+  {
+    "revision": "4c8fa600e536b166a400d94052917343",
+    "url": "/test-gitPage/static/media/school.4c8fa600.svg"
+  },
+  {
+    "revision": "58e41894e3710351b5f24e9eff74cb6c",
+    "url": "/test-gitPage/static/media/screely-1582668859108.58e41894.png"
+  },
+  {
+    "revision": "5c840ae6931610bb89c653fcc53531bd",
+    "url": "/test-gitPage/static/media/screely-1582668974156.5c840ae6.png"
+  },
+  {
+    "revision": "9df298cc09a24832f17f474455ba397c",
+    "url": "/test-gitPage/static/media/screen1.9df298cc.svg"
+  },
+  {
+    "revision": "42efb646fd249a2f56655072adab4cd1",
+    "url": "/test-gitPage/static/media/screen2.42efb646.svg"
+  },
+  {
+    "revision": "b817a34bfd38846eeca29a59efddb13f",
+    "url": "/test-gitPage/static/media/shape.b817a34b.png"
+  },
+  {
+    "revision": "00d39440a31a774581ed6d0387ca5383",
+    "url": "/test-gitPage/static/media/social copy.00d39440.svg"
+  },
+  {
+    "revision": "00d39440a31a774581ed6d0387ca5383",
+    "url": "/test-gitPage/static/media/social.00d39440.svg"
+  },
+  {
+    "revision": "9eb36da7fb090d4ff66ec5a06d172689",
+    "url": "/test-gitPage/static/media/sofa-white.9eb36da7.svg"
+  },
+  {
+    "revision": "16d88142c0e8292b4b1df3b829ede75e",
+    "url": "/test-gitPage/static/media/sofa.16d88142.svg"
+  },
+  {
+    "revision": "562818c66f2115606f132005b20f788b",
+    "url": "/test-gitPage/static/media/strzala.562818c6.svg"
+  },
+  {
+    "revision": "268b6ae26aa7d84c8b7855f77263306f",
+    "url": "/test-gitPage/static/media/troska.268b6ae2.mp4"
+  },
+  {
+    "revision": "6ee128493e1309eff3c50b1debce5f06",
+    "url": "/test-gitPage/static/media/vector-down.6ee12849.png"
+  },
+  {
+    "revision": "b9629e06c1fee6201e64ba9467441553",
+    "url": "/test-gitPage/static/media/venus-solid.b9629e06.svg"
+  },
+  {
+    "revision": "52d1f5307cf5e971880afeee65869bf8",
+    "url": "/test-gitPage/static/media/watch.52d1f530.jpeg"
+  }
+]);
